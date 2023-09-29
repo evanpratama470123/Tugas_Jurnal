36cbc2ee09e99f64481b395878caf65a095e8dd8
evanpratama470123 <pratamaevan2005@gmail.com>
2023-09-29 13:40:10 +0700

    initial commit

--- Jurnal01.java
+++ Jurnal01.java
@@ -1,6 +1,5 @@
 public class Jurnal01 {
     public static void main(String[] args) {
-       System.out.println("Nama : Evan Pratama" );
-       System.out.println("NIM : 607062330137");
+    
     }
 }
