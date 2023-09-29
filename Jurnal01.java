d72c203958711ebab960b88a3d803c6a8cf5c274
evanpratama470123 <pratamaevan2005@gmail.com>
2023-09-29 13:44:04 +0700

    identitas kelas, nama

--- Jurnal01.java
+++ Jurnal01.java
@@ -1,5 +1,7 @@
 public class Jurnal01 {
     public static void main(String[] args) {
-    
+    System.out.println("Nama : Evan Pratama");
+    System.out.println("NIM : 607061330137");
+    System.out.println("Kelas : D3-RPLA-01");
     }
 }
